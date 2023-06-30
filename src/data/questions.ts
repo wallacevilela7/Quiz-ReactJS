deffbc639e46e4c9a2465c62db22d7cdecf7c7db
Wallace Vilela <wallace.vilela9@gmail.com>
2023-06-30 11:20:34 -0300

    Estrutura da página + Type das questões

---
+++ src/data/questions.ts
@@ -0,0 +1,24 @@
+import { Question } from "@/types/Questions";
+
+export const questions : Question[] = [
+    {
+        question: "Qual ingrediente não pode faltar?",
+        options: [
+            "Farinha",
+            "Corante",
+            "Água",
+            "Açucar"
+        ],
+        answer: 1
+    },
+    {
+        question: "Melhor comida no domingo?",
+        options: [
+            "Macarronada",
+            "Lasanha",
+            "Churrasco",
+            "Strogonoff"
+        ],
+        answer: 1
+    }
+];
